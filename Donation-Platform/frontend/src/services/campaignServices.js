189b6a4cc0121b385e64491c0cf5c61aa1a4d03a
Md Ashfaqur Rahman Khan <ashfaqur.rahman.khan.zayed@gmail.com>
2025-10-08 01:24:57 +0600

    Add campaignServices.js for API calls

--- Donation-Platform/frontend/src/services/campaignServices.js
+++ Donation-Platform/frontend/src/services/campaignServices.js
@@ -2,7 +2,7 @@ import api from "./api";
 
 export const getCampaigns = async () => {
   try {
-    const res = await api.get("/api/campaigns");
+    const res = await api.get("/campaigns");
     return res.data;
   } catch (error) {
     console.error("Error fetching campaigns:", error);
